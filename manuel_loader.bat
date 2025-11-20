0388843c168050d4122f88ed7849e98d637f22b5
Erdem Bezmen <125645523+Powerss1@users.noreply.github.com>
2025-11-20 10:33:13 +0300

    Create manuel_loader.bat

---
+++ manuel_loader.bat
@@ -0,0 +1,12 @@
+@echo off
+title Manuel Captcha Çözücü
+echo Manuel Loader.js calistiriliyor...
+echo.
+
+REM Node.js ile manual_loader.js dosyasini calistirir.
+node manual_loader.js
+
+echo.
+echo Loader gorevini tamamladi. Lutfen terminaldeki mesaji kontrol edin.
+echo Pencereyi kapatmak icin bir tusa basin.
+pause
