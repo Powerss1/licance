c3f344dfc4151ef7926636d0d7d89d4cadc9f867
Erdem Bezmen <125645523+Powerss1@users.noreply.github.com>
2025-11-30 20:27:51 +0300

    Update sef.js

--- sef.js
+++ sef.js
@@ -173,9 +173,6 @@ function startTool() {
         { name: 'Farm', file: 'bot.js' },
         { name: 'AFK', file: 'gitbot.js' },
         { name: 'AFK2', file: 'gitbot2.js' }, // <-- Yeni bot eklendi
-        { name: 'AFK3', file: 'gitbot3.js' }, // <-- Yeni bot eklendi
-        { name: 'AFK4', file: 'gitbot4.js' }, // <-- Yeni bot eklendi
-        { name: 'AFK5', file: 'gitbot5.js' } // <-- Yeni bot eklendi
     ];
 
     let farmProc;
@@ -242,6 +239,7 @@ function startTool() {
         }, 10000);
     }
 }
+
 
 
 
